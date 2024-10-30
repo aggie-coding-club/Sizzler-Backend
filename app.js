fa309c6d3eff68e57e228a66f921da3531e62c3e
Vincent Jacob Conlee <vincentconlee@tamu.edu>
2024-10-30 18:50:57 -0500

    nothing

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
-var postsRouter = require("./routes/posts")
+var postsRouter = require("./routes/posts");
+var testsRouter = require("./routes/tests");
 
 var app = express();
 
@@ -13,11 +14,13 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+//app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "TEST")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/posts", postsRouter);
+app.use("/tests", testsRouter);
 
 const port = 3000;
 app.listen(port, () => {
