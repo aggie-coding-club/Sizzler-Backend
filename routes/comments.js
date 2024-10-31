26358785e638d111cad2d85a289195a6964da33b
Arnav Karekar <arnavkarekar72034@gmail.com>
2024-10-30 19:18:58 -0500

    added create endpoint comments.js

--- routes/comments.js
+++ routes/comments.js
@@ -3,6 +3,35 @@ var router = express.Router();
 var supabase = require("../supabase");
 
 // Create
+router.post("/create/", async function (req, res, next) {
+  try {
+    // create
+    const {userID, postID, title, caption} = req.body;
+
+    const { data, error } = await supabase
+      .from('comments') 
+      .insert([
+        { 
+          user_id : userID,
+          post_id : postID,
+          title : title,
+          caption : caption,
+        }
+      ])
+      .select()
+    
+    console.log("Success")
+    if (error) {
+      console.error('Error creating posts:', error);
+      return res.status(500).send('Error creating posts');
+    }
+
+    res.json(data);
+  } catch (err){
+    console.error('Error in creating posts:', err);
+    res.status(500).send('Server error');
+  }
+});
 
 // Read all
 router.get("/read/", async function (req, res, next) {
@@ -82,8 +111,8 @@ router.get("/read/post/:post_id", async function (req, res, next) {
     }
 });
 
-
 // Update
+
 
 // Delete
 
