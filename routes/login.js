3429e7bff7c438da7513e545f515f18939f83b36
yojoe56 <jessecool56@gmail.com>
2024-11-24 16:46:08 -0600

    login with customer profile

--- routes/login.js
+++ routes/login.js
@@ -31,15 +31,36 @@ router.post("/", async function (req, res, next) {
     //   { expiresIn: '1h' } // Token expires in 1 hour
     // );
     // user id = data.id
+    let { data: user_profiles, error: up_error } = await supabase
+        .from('user_profiles')
+        .select('*')
+        .eq('id', data.user.id) // Filter: 'id' equals 123
+        .single(); 
     
+    if (up_error){
+      return res.status(400).json({ up_error: up_error.message });
+    }
+    if(user_profiles.user_type === "customer"){
+      let { data: customers, error: c_error } = await supabase
+      .from('customers')
+      .select('*')
+          .eq('id', user_profiles.id) // Filter: 'id' equals 123
+          .single(); 
+      
+      if (c_error){
+        return res.status(400).json({ c_error: c_error.message });
+      }
+      return res.status(200).json({customers});
+    }
+
     
     // // Send token and success message back to the client
-    res.status(200).json({
-      success: true,
-      message: "Login successful!",
-      //token: token, // Include JWT token
-      user: data.user,
-    });
+    // res.status(200).json({
+    //   success: true,
+    //   message: "Login successful!",
+    //   //token: token, // Include JWT token
+    //   user: data.user,
+    // });
     //res.status(200).json({ message: 'Login successful! Check your email for verification.' });
   } catch (error) {
     res.status(500).json({ error: 'An error occurred during login' });
